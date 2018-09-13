2b616f9cbb97176e8bde8947cfeccd2d98b3cb75
Antony <antony.correoso@laposte.net>
2018-09-13 12:19:29 +0200

    Fin TP 10

---
+++ src/main/java/com/training/spring/bigcorp/config/MonitoredAspect.java
@@ -0,0 +1,20 @@
+package com.training.spring.bigcorp.config;
+
+import com.training.spring.bigcorp.service.SiteService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MonitoredAspect {
+
+    private final static Logger logger = LoggerFactory.getLogger(SiteService.class);
+    @Before("@annotation(com.training.spring.bigcorp.config.Monitored)")
+    public void logServiceBeforeCall(JoinPoint jp) {
+        logger.info("Appel finder {}", jp.getSignature());
+    }
+}
---
+++ src/main/java/com/training/spring/bigcorp/service/CaptorServiceImpl.java
@@ -0,0 +1,39 @@
+package com.training.spring.bigcorp.service;
+
+import com.training.spring.bigcorp.config.Monitored;
+import com.training.spring.bigcorp.model.Captor;
+import com.training.spring.bigcorp.service.measure.MeasureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class CaptorServiceImpl implements CaptorService{
+
+    private MeasureService fixedMeasureService;
+    private MeasureService realMeasureService;
+    private MeasureService simulatedMeasureService;
+
+    public CaptorServiceImpl(){}
+
+    @Autowired
+    public CaptorServiceImpl(@Qualifier("fixedMeasureService") MeasureService fixMeasService, MeasureService realMeasureService, MeasureService simulatedMeasureService) {
+        this.fixedMeasureService = fixedMeasureService;
+        this.realMeasureService = realMeasureService;
+        this.simulatedMeasureService = simulatedMeasureService;
+    }
+
+    @Override
+    @Monitored
+    public Set<Captor> findBySite(String siteId) {
+        Set<Captor> captors = new HashSet<>();
+        if (siteId == null) {
+            return captors;
+        }
+        captors.add(new Captor("Capteur A", null));
+        return captors;
+    }
+}
