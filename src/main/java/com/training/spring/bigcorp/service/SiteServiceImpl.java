364e3cd7d70f8f4120f7b16254e914bfa1ef9cd3
Antony <antony.correoso@laposte.net>
2018-09-17 11:56:12 +0200

    Amélioration couverture des tests

--- src/main/java/com/training/spring/bigcorp/service/SiteServiceImpl.java
+++ src/main/java/com/training/spring/bigcorp/service/SiteServiceImpl.java
@@ -2,30 +2,26 @@ package com.training.spring.bigcorp.service;
 
 import com.training.spring.bigcorp.config.Monitored;
 import com.training.spring.bigcorp.model.Site;
+import com.training.spring.bigcorp.repository.SiteDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
+import java.util.Optional;
 
 @Service
 public class SiteServiceImpl implements SiteService {
     private final static Logger logger = LoggerFactory.getLogger(SiteService.class);
+    private SiteDao siteDao;
     private CaptorService captorService;
-    @Autowired
-    private ResourceLoader resourceLoader;
 
-    public SiteServiceImpl() {
-    }
+    public SiteServiceImpl() {}
 
     @Autowired
-    public SiteServiceImpl(CaptorService captorService) {
+    public SiteServiceImpl(SiteDao siteDao, CaptorService captorService) {
         logger.debug("Init SiteServiceImpl :{}", this);
+        this.siteDao = siteDao;
         this.captorService = captorService;
     }
     @Override
@@ -36,9 +32,12 @@ public class SiteServiceImpl implements SiteService {
             return null;
         }
 
-        Site site = new Site("Florange");
-        site.setId(siteId);
-        site.setCaptors(captorService.findBySite(siteId));
+        Optional<Site> Optsite = siteDao.findById(siteId);
+        if(!Optsite.isPresent()){
+            return null;
+        }
+        Site site = Optsite.get();
+        //site.setCaptors(captorService.findBySite(siteId));
         return site;
     }
 }
---
+++ src/test/java/com/training/spring/bigcorp/service/SiteServiceImplTest.java
@@ -0,0 +1,76 @@
+package com.training.spring.bigcorp.service;
+
+import com.training.spring.bigcorp.model.Captor;
+import com.training.spring.bigcorp.model.FixedCaptor;
+import com.training.spring.bigcorp.model.Site;
+import com.training.spring.bigcorp.repository.CaptorDao;
+import com.training.spring.bigcorp.repository.SiteDao;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Set;
+
+public class SiteServiceImplTest {
+    @Mock
+    private SiteDao siteDao;
+    @Mock
+    private CaptorDao captorDao;
+    @InjectMocks
+    private SiteServiceImpl siteService;
+    @InjectMocks
+    private CaptorServiceImpl captorService;
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void findByIdShouldReturnNullWhenIdIsNull() {
+        // Initialisation
+        String siteId = null;
+        // Appel du SUT
+        Site site = siteService.findById(siteId);
+        // Vérification
+        Assertions.assertThat(site).isNull();
+    }
+    @Test
+    public void findById() {
+        // Initialisation
+        String siteId = "siteId";
+        Optional<Site> siteA = Optional.of(new Site("Site A"));
+        FixedCaptor expectedCaptor = new FixedCaptor("Capteur A", new Site("Florange"));
+        Mockito.when(siteDao.findById(siteId)).thenReturn(siteA);
+        Mockito.when(captorDao.findBySiteId(siteId)).thenReturn(Arrays.asList(expectedCaptor));
+
+        // Appel du SUT
+        Site site =  siteService.findById(siteId);
+        Set<Captor> captors = captorService.findBySite(siteId);
+        site.setCaptors(captors);
+
+        // Vérification
+        Assertions.assertThat(site)
+                .extracting(Site::getName)
+                .containsExactly("Site A");
+
+        Assertions.assertThat(site)
+                .extracting(Site::getCaptors)
+                .hasSize(1);
+    }
+
+    @Test
+    public void findByIdShouldReturnNullWhenIsUnknow(){
+        // Initialisation
+        String siteId = "blablablablablal";
+        // Appel du SUT
+        Site site = siteService.findById(siteId);
+        // Vérification
+        Assertions.assertThat(site).isNull();
+    }
+}
